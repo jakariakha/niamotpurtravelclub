0d1c52b1e8b70442f67ddd54b38d5747740350f6
Jakaria Khan <151080076+jakariakha@users.noreply.github.com>
2025-07-28 21:46:41 +0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,19 +2,19 @@
 
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'ntc_members');
-define('DB_PASS', 'Ov[BFZMH#}3Cej].}CHse%b)J');
-define('DB_NAME', 'ntc');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 
 // Database Connection
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // SMTP Configuration
-$hostname = 'smtp-relay.brevo.com';
-$username = '7a9932002@smtp-brevo.com';
-$password = 'h7PLtgjQzndrWAfY';
-$mail_from= 'no-reply@ntc.khapayplus.live';
-$mail_from_name = 'NTC';
+$hostname = '';
+$username = '';
+$password = '';
+$mail_from= '';
+$mail_from_name = '';
 
 define('MAIL_HOST', $hostname);
 define('MAIL_PORT', 587);
